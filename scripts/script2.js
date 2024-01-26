3fae3f2feef2a763820b3edf170a6ebdd9c9da03
bereket <bekidereje2@gmail.com>
2024-01-26 21:59:10 +0300

    added the highlighter when clicked

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,38 +1,71 @@
-materialsb('1','rook')
-materialsb('2','knight')
-materialsb('3','bishop')
-materialsb('4','queen')
-materialsb('5','king')
-materialsb('6','bishop')
-materialsb('7','knight')
-materialsb('8','rook')
+materialsb('1','rook','rook-b-l')
+materialsb('2','knight','knight-b-l')
+materialsb('3','bishop','bishop-b-l')
+materialsb('4','queen','queen-b')
+materialsb('5','king','king-b')
+materialsb('6','bishop','bishop-b-r')
+materialsb('7','knight','knight-b-r')
+materialsb('8','rook','rook-b-r')
 
 for (i=9;i<=16;i++){
-  materialsb(`${i}`,'pawn')
+  materialsb(`${i}`,'pawn',`pawn-b-${i-8}`)
 }
 
-materialsw('64','rook')
-materialsw('63','knight')
-materialsw('62','bishop')
-materialsw('61','king')
-materialsw('60','queen')
-materialsw('59','bishop')
-materialsw('58','knight')
-materialsw('57','rook')
+materialsw('64','rook','rook-w-r')
+materialsw('63','knight','knight-w-r')
+materialsw('62','bishop','bishop-w-r')
+materialsw('61','king','king-w')
+materialsw('60','queen','queen-w')
+materialsw('59','bishop','bishop-w-l')
+materialsw('58','knight','knight-w-l')
+materialsw('57','rook','rook-w-l')
 
 
 for (i=49;i<=56;i++){
-  materialsw(`${i}`,'pawn')
+  materialsw(`${i}`,'pawn',`pawn-w-${i-48}`)
 }
 
 
 
-function materialsw(cell,ic){
-  document.querySelector(`.js-cell-${cell}`).innerHTML = `<button class="icons"><i class="fa-solid fa-chess-${ic} fa-2xl" style="color: #909192;"></i></button>`
+function materialsw(cell,ic,cls){
+  document.querySelector(`.js-cell-${cell}`).innerHTML = `<button class="icons ${cls}"><i class="fa-solid fa-chess-${ic} fa-2xl" style="color: #909192;"></i></button>`;
+  document.querySelector(`.${cls}`).addEventListener("click",()=>{
+    document.querySelector(`.js-cell-${cell}`).classList.add("sp-cell")
+    for(l=1;l<=64;l++){
+      if(l === Number(`${cell}`)){
+        continue
+      }else{
+        document.querySelector(`.js-cell-${l}`).classList.remove("sp-cell")
+      }
+      
+    }
+  })
+ 
 }
 
 
-function materialsb(cell,ic){
-  document.querySelector(`.js-cell-${cell}`).innerHTML = `<button class="icons js-icons"><i class="fa-solid fa-chess-${ic} fa-2xl" style="color: #000000;"></i></button>`
+function materialsb(cell,ic,cls){
+  document.querySelector(`.js-cell-${cell}`).innerHTML = `<button class="icons ${cls}"><i class="fa-solid fa-chess-${ic} fa-2xl" style="color: #000000;"></i></button>`;
+  document.querySelector(`.${cls}`).addEventListener("click",()=>{
+    document.querySelector(`.js-cell-${cell}`).classList.add("sp-cell")
+    for(i=1;i<=64;i++){
+      if(i === Number(`${cell}`)){
+        continue
+      }else{
+        document.querySelector(`.js-cell-${i}`).classList.remove("sp-cell")
+      }
+      
+    }
+  })
 }
+/*
+click('rook-b-l','1')
 
+function click(cls,cell){
+document.querySelector(`.${cls}`).addEventListener("click",()=>{
+  document.querySelector(`.js-cell-${cell}`).classList.add("sp-cell")
+})}
+
+document.querySelector(`.rook-b-l`).addEventListener("click",()=>{
+  document.querySelector(`.js-cell-1`).classList.add("sp-cell")
+})*/
