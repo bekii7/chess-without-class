a9d76ad77ad0e52fb94996c4ee4cb67b36ce9be9
bereket <bekidereje2@gmail.com>
2024-02-02 21:28:15 +0300

    possible move fixed

--- scripts/script2.js
+++ scripts/script2.js
@@ -9,36 +9,43 @@ let move={
 
   
 //the move for the pawn
-movePawn('pawn-b-1',`17`,`9` )
-movePawn('pawn-b-2','18','10' )
-movePawn('pawn-b-3','19','11' )
-movePawn('pawn-b-4','20','12' )
-movePawn('pawn-b-5','21','13' )
-movePawn('pawn-b-6','22','14' )
-movePawn('pawn-b-7','23','15' )
-movePawn('pawn-b-8','24','16' )
+movePawn('pawn-b-1',`17`,`9`,'#000000' )
+movePawn('pawn-b-2','18','10','#000000')
+movePawn('pawn-b-3','19','11','#000000')
+movePawn('pawn-b-4','20','12','#000000')
+movePawn('pawn-b-5','21','13','#000000')
+movePawn('pawn-b-6','22','14','#000000')
+movePawn('pawn-b-7','23','15','#000000')
+movePawn('pawn-b-8','24','16','#000000')
 
 
-movePawn('pawn-w-1','33','49', )
-movePawn('pawn-w-2','34','50', )
-movePawn('pawn-w-3','35','51', )
-movePawn('pawn-w-4','36','52',)
-movePawn('pawn-w-5','37','53' )
-movePawn('pawn-w-6','38','54' )
-movePawn('pawn-w-7','39','55')
-movePawn('pawn-w-8','40','56')
-
-
-
+movePawn('pawn-w-1','33','49','#909192')
+movePawn('pawn-w-2','34','50','#909192')
+movePawn('pawn-w-3','35','51','#909192')
+movePawn('pawn-w-4','36','52','#909192')
+movePawn('pawn-w-5','37','53','#909192')
+movePawn('pawn-w-6','38','54','#909192')
+movePawn('pawn-w-7','39','55','#909192')
+movePawn('pawn-w-8','40','56','#909192')
 
 
 
-function movePawn(pawn,nextMove,previousMove){
+
+
+
+function movePawn(pawn,nextMove,previousMove,color){
   
   document.querySelector(`.${pawn}`).addEventListener("click",()=>{
+    if(document.querySelector('.show-move')){
+      document.querySelector(`.js-show-move`).remove()
+    }
+    if(document.querySelector('.show-move')){
+      document.querySelector(`.js-show-move`).remove()
+    }
 
 
-    console.log(11)
+
+    
     specialCell(`${previousMove}`)
   
 
@@ -51,37 +58,26 @@ function movePawn(pawn,nextMove,previousMove){
 
 
     document.querySelector(`.js-cell-${nextMove} .js-show-move`).addEventListener("click",()=>{
-       showmove(`${nextMove}`,`${previousMove}`,`${pawn}`)
+       showmove(`${nextMove}`,`${previousMove}`,`${pawn}`,`${color}`)
     })
     document.querySelector(`.js-cell-${Number(nextMove)+8} .js-show-move`).addEventListener("click",()=>{
-      showmove(`${Number(nextMove)+8}`,`${previousMove}`,`${pawn}`)
+      showmove(`${Number(nextMove)+8}`,`${previousMove}`,`${pawn}`,`${color}`)
     })
     });
     
       
 }
-function showmove(cellA,cellR,pawn){
-  if (cellA)
-  materials(`${cellA}`,'pawn',`${pawn}`)
+
+
+function showmove(cellA,cellR,pawn,color){
+  
+  materials(`${cellA}`,'pawn',`${pawn}`,`${color}`)
   document.querySelector(`.js-cell-${cellR} .${pawn}`).remove()
-
+  document.querySelector(`.js-show-move`).remove()
   
   
 
-  if(cellA >= 17 && cellA <= 24){
-    document.querySelector(`.js-cell-${Number(cellA)+8}`).innerHTML = ''
-
-
-  }else if(cellA >= 25 && cellA <= 32){
-    document.querySelector(`.js-cell-${Number(cellA)-8}`).innerHTML = ''
-  }
-  if(cellA >= 33 && cellA <= 40){
-    document.querySelector(`.js-cell-${Number(cellA)-8}`).innerHTML = ''
-
-
-  }else if(cellA >= 41 && cellA <= 48){
-    document.querySelector(`.js-cell-${Number(cellA)+8}`).innerHTML = ''
-  }
+  
 
 }
 function specialCell(cell){
